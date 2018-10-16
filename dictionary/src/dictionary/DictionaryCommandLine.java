675db8cfb39c78092822f2cf0fd10de24a4d2089
thjenhop <thjenhop99@gmail.com>
2018-10-16 14:57:43 +0700

    Hop update BTL_Commandline

---
+++ dictionary/src/dictionary/DictionaryCommandLine.java
@@ -0,0 +1,139 @@
+package dictionary;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DictionaryCommandLine {
+	DictionaryManagement manager = new DictionaryManagement();
+	
+	public static void main(String[] args) throws IOException {
+		DictionaryCommandLine x = new DictionaryCommandLine();
+		//x.dictionaryBasic();
+		//x.dictionaryAdvanced();
+		//x.manager.insertFromFile();
+		//x.showAllWord();
+		//x.dictionarySearch();
+		//x.manager.insertFromFile();
+		//x.showAllWord();
+	}
+	
+	public void run() throws IOException {
+		System.out.println("1.Chức năng cơ bản");
+		System.out.println("2.Chức năng nâng cao");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Nhập lựa chọn : ");
+		int choise = input.nextInt();
+		switch(choise) {
+			case 1: dictionaryBasic();break;
+			case 2: dictionaryAdvanced();break;
+		}
+		
+		System.out.print("Muốn xuất dữ liệu hiện tại ra file (y/n):");
+		String x = input.next();
+		if(x.equals("y")) manager.exportToFile();
+	}
+	
+	public void showAllWord_advanced() {  // in tu - phien am - nghia
+		System.out.print("\n");
+		
+		for( int i = 0 ; i < manager.dict.data.size(); i++)
+		{
+			System.out.println("từ thứ "+ (i + 1));
+			System.out.println( "từ : " +manager.dict.data.get(i).getWord_target() + "\t/"+ manager.dict.data.get(i).getWord_pronoun()+"/" );
+			System.out.println("nghĩa :\n" + manager.dict.data.get(i).getWord_explain());
+			System.out.print("\n");
+		}
+	}
+	public void dictionaryBasic() {
+		Scanner input = new Scanner(System.in);
+		int choise;
+		do {
+			System.out.println("1.Nhập từ bàn phím");
+			System.out.println("2.Hiện tất cả từ");
+			System.out.print("Nhập lựa chọn : ");
+			choise = input.nextInt();
+			switch(choise) {
+				case 1: manager.insertFromCommandLine();break;
+				case 2: showAllWord_basic();break;
+			}
+			System.out.print("Muốn tiếp tục(1_y/2_n) : ");
+			choise= input.nextInt();
+		}while ( choise == 1);
+	}
+	public void showAllWord_basic() {// chi in tu va nghia
+		System.out.print("\n");
+		
+		for( int i = 0 ; i < manager.dict.data.size(); i++)
+		{
+			System.out.println("từ thứ "+ (i + 1));
+			System.out.println( "từ : " +manager.dict.data.get(i).getWord_target() );
+			System.out.println("nghĩa :\n" + manager.dict.data.get(i).getWord_explain());
+			System.out.print("\n");
+		}
+	}
+	
+	public void dictionaryAdvanced() throws IOException {
+		Scanner input = new Scanner(System.in);
+		int choise;
+		do {
+			System.out.println("1.Lấy dữ liệu từ file(bắt buộc)");
+			System.out.println("2.Hiện tất cả từ");
+			System.out.println("3.Tìm kiếm từ");
+			System.out.println("4.Tra nghĩa từ");
+			System.out.println("5.Lựa chọn khác");
+			System.out.print("Nhập lựa chọn : ");
+			int choise_1 = input.nextInt();
+			switch(choise_1) {
+				case 1: manager.insertFromFile();  break;
+				case 2: showAllWord_advanced();             break;
+				case 3: dictionarySearch();        break;
+				case 4: manager.dictionaryLookup();break;
+				case 5:{
+					System.out.println("1.Thêm từ");
+					System.out.println("2.Sửa từ");
+					System.out.println("3.Xóa từ");
+					System.out.print("Nhập lựa chọn : ");
+					int choise_2 = input.nextInt();
+					switch(choise_2) {
+					case 1: manager.addData();     break;
+					case 2: manager.insertData();  break;
+					case 3: manager.deleteData();  break;
+					}
+				}break;
+			}
+			System.out.print("Muốn tiếp tục(1_y/2_n) : ");
+			choise= input.nextInt();
+		}while ( choise == 1);
+	}
+	
+	public void dictionarySearch() {
+		boolean _continue = true;
+		Scanner input = new Scanner(System.in);
+		do {
+			System.out.print("Từ cần tìm : ");
+			String x = input.nextLine().toLowerCase();
+			for ( int i = 0; i< manager.dict.data.size(); i++) {
+				if ( manager.dict.data.get(i).word_target.length() >= x.length()) { // do dai target >= do dai tu can tim
+					String word_split = splitString(manager.dict.data.get(i).word_target, x.length());
+					if( word_split.equals(x)) {
+						System.out.println(manager.dict.data.get(i).word_target);
+					}
+				}
+			}
+			System.out.println("Muốn tiếp tục tìm kiếm (1_y/2_n) : ");
+			int y = input.nextInt();
+			input.nextLine();
+			if( y == 2) _continue = false;
+		}while(_continue);
+		System.out.print("Muốn tra nghĩa (1_y/2_n) : ");
+		int z = input.nextInt();
+		if( z == 1 ) manager.dictionaryLookup();
+	}
+	public static String splitString(String s, int n) { 
+		String x = "";
+		for ( int i = 0; i< n; i++) {
+			x += s.charAt(i);
+		}
+		return x;
+	}
+}
---
+++ dictionary/src/dictionary/DictionaryManagement.java
@@ -0,0 +1,161 @@
+package dictionary;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.util.*;
+
+public class DictionaryManagement {
+	protected Dictionary dict = new Dictionary();
+	protected String file = "dictionary.txt";
+	
+	public void insertArrayWord_target() {
+		if( dict.data_word_target.size() != 0 ) {	
+			dict.data_word_target.clear();
+		}
+		for ( int i = 0; i< dict.data.size(); i++) { // add het word_target vao 
+			dict.data_word_target.add(dict.data.get(i).getWord_target()); 
+		}
+	}
+	public void insertFromCommandLine() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Nhập số từ : ");
+		int numWord = input.nextInt();
+		input.nextLine();
+		for( int i = 0; i< numWord; i++) {
+			System.out.println("Nhập từ thứ "+ (i+1) );
+			System.out.print("Nhập từ : ");
+			String target = input.nextLine().toLowerCase();
+			
+			System.out.print("Nhập nghĩa : ");
+			String explain = input.nextLine().toLowerCase();
+			dict.addWord(target, explain);
+		}
+	}
+	
+	
+	
+	public void insertFromFile() throws IOException {
+		long t0= System.currentTimeMillis();
+		BufferedReader input = new BufferedReader(new FileReader(file));
+		int i;
+		int j = 0;
+		Word newWord = new Word();
+		while ( (i = input.read()) != -1 ) {
+			if( i == 10 ) { // la ki tu carriage return or 13
+				dict.data.add(newWord);
+				newWord = new Word();
+			}
+			else {
+				//if( i != 10) {
+				String x =input.readLine();
+					if( (char)i == '@' ) {// la target + pronoun
+						x = x.trim();
+						String[] y = x.split(" /", 2);
+						newWord.setWord_target(y[0].trim());
+						newWord.setWord_pronoun( y[1].substring(0, y[1].length() -1).trim());
+					}
+					else {
+						if( newWord.getWord_explain() == "" ) newWord.setWord_explain( (char)i+ x);
+						else {
+							String y = newWord.getWord_explain()+ "\n" + (char)i + x;
+							newWord.setWord_explain(y);
+						}
+					}
+				//}
+			}
+
+		}
+		dict.data.add(newWord);
+		System.out.println("done!");	
+		long t2= System.currentTimeMillis();
+		//System.out.println(t2-t0);
+		
+	}
+	
+	public void dictionaryLookup() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Nhập từ muốn tìm kiếm : ");
+		String s = input.nextLine(); 
+		s= s.toLowerCase();
+		for ( int i = 0; i< dict.data.size(); i++) {
+			if(dict.data.get(i).getWord_target().equals(s) ) {
+				System.out.println(dict.data.get(i).getWord_explain());
+				return;
+			}
+		}
+		System.out.println("Từ không tồn tại");
+	}
+	
+	public void addData() {
+		insertArrayWord_target();
+		Scanner input = new Scanner(System.in);
+		System.out.print("Nhập từ tiếng anh : ");
+		
+		String target = input.nextLine().toLowerCase();
+		if( dict.data_word_target.contains(target) ) {
+			System.out.println("Từ đã tồn tại !");
+		}
+		else {
+			System.out.print("Nhập nghĩa : ");
+			String explain = input.nextLine().toLowerCase();
+			dict.addWord(target, explain);
+		}
+
+	}
+	
+	public void insertData() {
+		insertArrayWord_target();
+		Scanner input = new Scanner(System.in);
+		System.out.print("Nhập từ tiếng anh cần sửa : ");
+		String target = input.nextLine().toLowerCase();
+		for ( int i = 0; i< dict.data.size();i++) {
+			if(dict.data.get(i).getWord_target().equals(target)) {
+				System.out.print("Nhập phiên âm :");
+				String pronoun = input.nextLine().toLowerCase();
+				dict.data.get(i).setWord_pronoun(pronoun);
+				System.out.print("Nhập nghĩa : ");
+				String explain = input.nextLine().toLowerCase();
+				dict.data.get(i).setWord_explain(explain);
+				return;
+			}
+			else {
+				System.out.println("Từ không tồn tại");
+			}
+		}
+
+	}
+	
+	public void deleteData() {
+		System.out.print("Nhập từ cần xóa : ");
+		Scanner input = new Scanner(System.in);
+		String target = input.nextLine().toLowerCase();
+		for( int i = 0; i< dict.data.size();i++) {
+			if ( dict.data.get(i).getWord_target().equals(target) ) {
+				dict.data.remove(i);
+				input.close();
+				return;
+			}
+			else {
+				System.out.println("Từ không tồn tại!");
+			}
+		}
+
+	}
+	
+	public void exportToFile() throws IOException {
+		FileWriter output = new FileWriter(file);
+		for ( int i = 0; i< dict.data.size() ; i++) {
+			output.write("@"+dict.data.get(i).getWord_target() + "/" + dict.data.get(i).getWord_pronoun() +"/");
+			output.write("\n");
+			output.write(dict.data.get(i).getWord_explain());
+			if( i != dict.data.size()-1 ) output.write("\n\n"); // khong phai tu cuoi cung thi cach ra 1 dong
+		}
+		output.close();
+		
+	}
+}
